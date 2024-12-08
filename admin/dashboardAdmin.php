81aae34ed0716a06ffba06c2088eae7145e7711b
valinadz <valinaadzraa@gmail.com>
2024-12-08 23:38:32 +0700

    di dsbrd udh keubah namanya, tp yg data" msih alex namanya

--- admin/dashboardAdmin.php
+++ admin/dashboardAdmin.php
@@ -13,7 +13,6 @@ if (!isset($_SESSION['user_id']) || $_SESSION['level'] != 1) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Dashboard Admin</title>
-    <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
@@ -25,20 +24,13 @@ if (!isset($_SESSION['user_id']) || $_SESSION['level'] != 1) {
     <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
     <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
     
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <style>
       .main-header .navbar {
-          background-color: #115599 !important; /* Mengganti warna navbar */
+          background-color: #115599 !important;
       }
   </style>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
-<!-- Site wrapper -->
 <div class="wrapper">
 
     <header class="main-header">
@@ -63,7 +55,7 @@ if (!isset($_SESSION['user_id']) || $_SESSION['level'] != 1) {
                     <img src="../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
-                    <p>Alexander Pierce</p>
+                    <p><?php echo htmlspecialchars($nama_admin); ?></p>
                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
             </div>
@@ -78,7 +70,6 @@ if (!isset($_SESSION['user_id']) || $_SESSION['level'] != 1) {
             </ul>
         </section>
     </aside>
-
 
     <div class="content-wrapper">
         <section class="content-header">
@@ -108,19 +99,12 @@ if (!isset($_SESSION['user_id']) || $_SESSION['level'] != 1) {
     </footer>
 
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery 2.2.3 -->
 <script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
 <script src="../bootstrap/js/bootstrap.min.js"></script>
-<!-- SlimScroll -->
 <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
 <script src="../plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
 <script src="../dist/js/app.min.js"></script>
-<!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
 </body>
 </html>
