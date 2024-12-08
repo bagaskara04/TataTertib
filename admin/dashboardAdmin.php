ecfa2be0f0627470e629aa7725927eb83bdef1bd
karalion9 <sagabdespair01@gmail.com>
2024-12-08 10:24:33 +0700

    logout

--- admin/dashboardAdmin.php
+++ admin/dashboardAdmin.php
@@ -69,10 +69,12 @@ if (!isset($_SESSION['user_id']) || $_SESSION['level'] != 1) {
             </div>
             <ul class="sidebar-menu">
                 <li class="header">MAIN NAVIGATION</li>
-                <li class="active"><a href="dashboardAdmin.html"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-                <li class="active"><a href="dataMahasiswa.html"><i class="fa fa-users"></i> <span>Data Mahasiswa</span></a></li>
-                <li class="active"><a href="dataDosen.html"><i class="fa fa-users"></i> <span>Data Dosen</span></a></li>
-                <li class="active"><a href="dataLaporan.html"><i class="fa fa-file-text-o"></i> <span>Data Laporan Pelanggaran</span></a></li>
+                <li class="active"><a href="dashboardAdmin.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+                <li><a href="dataMahasiswa.html"><i class="fa fa-users"></i> <span>Data Mahasiswa</span></a></li>
+                <li><a href="dataDosen.html"><i class="fa fa-users"></i> <span>Data Dosen</span></a></li>
+                <li><a href="dataDPA.html"><i class="fa fa-file-text-o"></i> <span>Data DPA</span></a></li>
+                <li><a href="dataLaporan.html"><i class="fa fa-file-text-o"></i> <span>Data Laporan Pelanggaran</span></a></li>
+                <li><a href="../logout.php"><i class="fa fa-exit"></i><span>Log Out</span></a></li>
             </ul>
         </section>
     </aside>
