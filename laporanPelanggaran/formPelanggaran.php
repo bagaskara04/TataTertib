e4580b2eb5e9cf7e014f075616687a4b8affb72b
AnggaAhmad05 <anggaahmad9027@gmail.com>
2024-12-11 01:16:32 +0700

    dashboard

--- laporanPelanggaran/formPelanggaran.php
+++ laporanPelanggaran/formPelanggaran.php
@@ -252,7 +252,7 @@ $pelanggaran_list = getAllPelanggaran($conn);
     </div>
 
     <!-- Navbar -->  
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">  
+    <nav class="main-header navbar navbar-expand navbar-blue navbar-dark">  
         <ul class="navbar-nav">  
             <li class="nav-item">  
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button">
@@ -260,35 +260,33 @@ $pelanggaran_list = getAllPelanggaran($conn);
                 </a>  
             </li>  
             <li class="nav-item d-none d-sm-inline-block">  
-                <a href="#" class="nav-link">Home</a>  
+                <a href="#" class="nav-link"></a>  
             </li>  
         </ul>  
     </nav>  
 
     <!-- Sidebar -->  
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">  
-        <a href="#" class="brand-link">  
-            <span class="brand-text font-weight-light">SimpleAdminLTE</span>  
-        </a>  
-        <div class="sidebar">  
-            <nav class="mt-2">  
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">  
-                    <li class="nav-item">  
-                        <a href="#" class="nav-link active">  
-                            <i class="nav-icon fa fa-file"></i>  
-                            <p>Data Pengaduan</p>  
-                        </a>  
-                    </li>  
-                </ul>  
-            </nav>  
-        </div>  
-    </aside>  
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">  
+            <a href="#" class="brand-link">  
+                <span class="brand-text font-weight-light">SiTatib</span>  
+            </a>  
+            <div class="sidebar">  
+                <nav class="mt-2">  
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">  
+                        <li class="nav-item">  
+                        <li class="active"><a href="../dosen/dashboardDosen.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li> 
+                            </a>  
+                        </li>  
+                    </ul>  
+                </nav>  
+            </div>  
+        </aside>  
 
     <!-- Content Wrapper -->  
     <div class="content-wrapper">  
         <div class="content-header">  
             <div class="container-fluid">  
-                <h1 class="m-0">Data Pengaduan</h1>  
+                <h1 class="m-0">Laporan Pelanggaran</h1>  
             </div>  
         </div>  
 
@@ -346,7 +344,7 @@ $pelanggaran_list = getAllPelanggaran($conn);
                                     Status Pengaduan  
                                     <input type="hidden" id="status_pengaduan" name="status_pengaduan" value="proses">
                                     <div class="alert alert-info d-flex align-items-center" role="alert">
-                                        <span>Sedang Diproses</span>
+                                        <span>proses</span>
                                     </div>
                                 </div>
                             </div>
@@ -449,6 +447,14 @@ $pelanggaran_list = getAllPelanggaran($conn);
         </div>
     </div>
 </div>
+
+
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b><a href="https://jti.polinema.ac.id/" target="_blank">Jurusan Teknologi Informasi</a></b>
+        </div>
+        <strong><a href="https://polinema.ac.id" target="_blank">Politeknik Negeri Malang</a></strong>
+    </footer>
 
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
